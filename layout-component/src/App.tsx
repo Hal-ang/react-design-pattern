1507852bbfa7021a59484481d424264c92dfc77f
Hal-ang <jpjgv05@gmail.com>
2023-12-26 21:14:48 +0900

    Add SideComp background style

--- layout-component/src/App.tsx
+++ layout-component/src/App.tsx
@@ -3,11 +3,11 @@ import './index.css';
 import SplitScreen from './components/SplitScreen';
 
 const LeftSideComp = () => {
-  return <h2>I am left</h2>;
+  return <h2 style={{ backgroundColor: 'crimson' }}>I am left</h2>;
 };
 
 const RightSideComp = () => {
-  return <h2>I am right</h2>;
+  return <h2 style={{ backgroundColor: 'brown' }}>I am right</h2>;
 };
 
 function App() {
