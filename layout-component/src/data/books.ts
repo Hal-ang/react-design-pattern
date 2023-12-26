a7333202912a7c9894bf9dedb1c4ba189c0a791b
Hal-ang <jpjgv05@gmail.com>
2023-12-26 21:36:47 +0900

    Add mocking data

---
+++ layout-component/src/data/books.ts
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    name: 'To kill a Mockingbird',
+    pages: 281,
+    title: 'Harper Lee',
+    price: 12.99,
+  },
+  {
+    name: 'The Catche in the Rye',
+    pages: 224,
+    title: 'J.D. Salinger',
+    price: 9.99,
+  },
+  {
+    name: 'The Little Prince',
+    pages: 85,
+    title: 'Antoine de Saint-Exupery',
+    price: 7.99,
+  },
+];
