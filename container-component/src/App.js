ea19bf2ee7da9d25d56b264d63b6fdfa0436c6ff
Hal-ang <jpjgv05@gmail.com>
2023-12-27 14:50:30 +0900

    Implement DataSource

--- container-component/src/App.js
+++ container-component/src/App.js
@@ -1,17 +1,21 @@
-import { BookInfo } from './components/book-info';
-import ResourceLoader from './components/resource-loader';
+import DataSource from './components/data-source';
 import { UserInfo } from './components/user-info';
+import axios from 'axios';
+
+const getDataFromServer = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl={'/users/2'} resourceName={'user'}>
+      <DataSource
+        getData={() => getDataFromServer('/users/3')}
+        resourceName={'user'}
+      >
         <UserInfo />
-      </ResourceLoader>
-
-      <ResourceLoader resourceUrl={'/books/2'} resourceName={'book'}>
-        <BookInfo />
-      </ResourceLoader>
+      </DataSource>
     </>
   );
 }
