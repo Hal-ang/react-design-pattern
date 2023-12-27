8bc84d749929561d341bb125ffa23d9383d6d6a6
Hal-ang <jpjgv05@gmail.com>
2023-12-27 14:34:58 +0900

    Implement userLoader

--- container-component/src/App.js
+++ container-component/src/App.js
@@ -1,12 +1,12 @@
-import CurrentUserLoader from './components/current-user-loader';
 import { UserInfo } from './components/user-info';
+import UserLoader from './components/user-loader';
 
 function App() {
   return (
     <>
-      <CurrentUserLoader>
+      <UserLoader userId={3}>
         <UserInfo />
-      </CurrentUserLoader>
+      </UserLoader>
     </>
   );
 }
---
+++ container-component/src/components/user-loader.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+const UserLoader = ({ userId,children }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => { 
+    (async () => {
+      const response = await axios.get(`/users/${userId}`)
+      setUser(response.data)
+    }) ()
+  }, [userId])
+  console.log(user)
+  
+  return (
+    <>
+      {React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, {user})
+        }
+        return child
+      })}
+    </>
+  );
+};
+
+export default UserLoader;
