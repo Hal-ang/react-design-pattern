935cb89702c767b4de532bc43830bf90e34889f5
Hal-ang <jpjgv05@gmail.com>
2023-12-27 21:42:54 +0900

    Implement Modal.jsx

---
+++ controlled-and-uncontrolled-components/src/components/Modal.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const ModalBackground = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100%;
+`;
+
+const ModalContent = styled.div`
+  margin: 12% auto;
+  padding: 24px;
+  background-color: wheat;
+  width: 50%;
+`;
+
+export const Modal = ({ children }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShow(true)}>Show Modal</button>
+      {show && (
+        <ModalBackground onClick={() => setShow(false)}>
+          <ModalContent onClick={(e) => e.stopPropagation()}>
+            <button onClick={() => setShow(false)}>Hide Modal</button>
+            {children}
+          </ModalContent>
+        </ModalBackground>
+      )}
+    </>
+  );
+};
