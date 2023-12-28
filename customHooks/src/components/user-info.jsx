946c7e6113532269545e2fc629a03d93087218a5
Hal-ang <jpjgv05@gmail.com>
2023-12-28 22:38:09 +0900

    Implement data source hook

--- customHooks/src/components/user-info.jsx
+++ customHooks/src/components/user-info.jsx
@@ -1,8 +1,20 @@
-import { useResource } from './resource.hook';
-import { useUser } from './user.hook';
+import axios from 'axios';
+import { useDataSource } from './data-source.hook';
+
+const fetchFromServer = resourceUrl => async () => { 
+  const response = await axios.get(resourceUrl);
+  return response.data;
+}
+
+const getFromLocalStorage = key => () => {
+  return localStorage.getItem(key);
+}
 
 export const UserInfo = ({userId}) => {
-  const user = useResource(`/users/${userId}`);
+  // const user = useResource(`/users/${userId}`);
+  const user = useDataSource(fetchFromServer(`/users/${userId}`))
+  const message = useDataSource(getFromLocalStorage('msg'))
+  
   const { name, age, country, books } = user || {};
   
   return user ? (
